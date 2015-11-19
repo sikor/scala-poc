6fc4026adfddbc2906435d67d2509be3d06ced7d
sikor <p.sikora@avsystem.com>
2015-11-19 18:36:02 +0100

    Storing sessions per IP:PORT.

--- core/src/main/scala/streams/io/CoapSessions.scala
+++ core/src/main/scala/streams/io/CoapSessions.scala
@@ -3,13 +3,11 @@ package streams.io
 import java.net.InetSocketAddress
 import java.util
 
-import monifu.reactive.{Subscriber, Ack, Observable}
-import streams.bidi.BidiStream.ProcessingAction
+import streams.bidi.BidiStream.{PushToOutput, NoAction, ProcessingAction, PushToBoth}
 import streams.bidi.{BidiProcessor, InOut}
 import streams.io.CoapSessions.{CoapClient, CoapSession}
 import streams.io.UdpStream.Datagram
-
-import scala.concurrent.Future
+import monifu.reactive._
 
 
 /**
@@ -19,17 +17,20 @@ object CoapSessions {
   type CoapMsg = Any
 
 
-  class CoapSession(val address: InetSocketAddress) extends InOut[CoapMsg, CoapMsg] {
-    override def onSubscribe(subscriber: Subscriber[CoapMsg]): Unit = ???
+  class CoapSession(val address: InetSocketAddress, val initMsg: CoapMsg) {
 
-    override def onError(ex: Throwable): Unit = ???
+    def stream(): InOut[CoapMsg, CoapMsg] = ???
 
-    override def onComplete(): Unit = ???
+    def outDatagrams: Observable[Datagram] = ???
 
-    override def onNext(elem: CoapMsg): Future[Ack] = ???
+    def inDatagrams: Channel[Datagram] = ???
+
+
   }
 
-  class CoapClient(val stream: InOut[CoapMsg, CoapMsg], val address: InetSocketAddress)
+  class CoapClient(val stream: InOut[CoapMsg, CoapMsg], val targetAddress: InetSocketAddress, val initMsg: CoapMsg) {
+
+  }
 
 }
 
@@ -37,7 +38,29 @@ class CoapSessions extends BidiProcessor[Datagram, CoapSession, CoapClient, Obse
 
   private val sessions = new util.HashMap[InetSocketAddress, CoapSession]
 
-  override def onInputMessage(inputMsg: Datagram): ProcessingAction[CoapSession, Observable[Datagram]] = ???
+  override def onInputMessage(datagram: Datagram): ProcessingAction[CoapSession, Observable[Datagram]] = {
+    val oldSession = sessions.get(datagram.address)
+    if (oldSession != null) {
+      oldSession.inDatagrams.pushNext(datagram)
+      NoAction
+    } else {
+      val session = new CoapSession(datagram.address, datagram)
+      sessions.put(datagram.address, session)
+      PushToBoth(session, session.outDatagrams)
+    }
+  }
 
-  override def onOutputMessage(outputMsg: CoapClient): ProcessingAction[CoapSession, Observable[Datagram]] = ???
+  override def onOutputMessage(client: CoapClient): ProcessingAction[CoapSession, Observable[Datagram]] = {
+    val oldSession = sessions.get(client.targetAddress)
+    if (oldSession != null) {
+      client.stream.onError(new IllegalStateException("Session with this address already exists: " + client.targetAddress))
+      NoAction
+    } else {
+      val session = new CoapSession(client.targetAddress, client.initMsg)
+      sessions.put(client.targetAddress, session)
+      session.stream().subscribe(client.stream)
+      client.stream.subscribe(session.stream())
+      PushToOutput(session.outDatagrams)
+    }
+  }
 }
