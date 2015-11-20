f869b0ccd00b3b77788267027c26ead2b72c55c7
Paweł Sikora <siegfriedek@gmail.com>
2015-11-20 01:45:12 +0100

    Coap api draft.

--- core/src/main/scala/streams/io/CoapSessions.scala
+++ core/src/main/scala/streams/io/CoapSessions.scala
@@ -4,6 +4,7 @@ import java.net.InetSocketAddress
 import java.util
 
 import monifu.concurrent.Scheduler
+import monifu.reactive.observers.SynchronousObserver
 import streams.bidi.BidiStream.{PushToOutput, NoAction, ProcessingAction, PushToBoth}
 import streams.bidi.{BidiProcessor, InOut}
 import streams.io.CoapSessions.{CoapClient, CoapSession}
@@ -18,21 +19,36 @@ object CoapSessions {
   type CoapMsg = Any
 
 
+  /**
+    *
+    * When this session is closed it won't produce any new messages. It won't accept any new datagrams.
+    * So when it signalize onComplete than subscriber can be released. New session for this address will be created.
+    * It takes the stream of clients. Each has associated token for message filtering. Block-wise handling is invisible
+    * for client. Server for all requests can be registered. Observation Responses without associated client can be
+    * accessed separately.
+    *
+    * @param address associated target address
+    * @param initMsg message which initiated the session.
+    */
   class CoapSession(val address: InetSocketAddress, val initMsg: CoapMsg) {
 
-    def stream(): InOut[CoapMsg, CoapMsg] = ???
+    def requests(): Observable[InOut[CoapMsg, CoapMsg]] = ???
 
+    def clients(): SynchronousObserver[InOut[CoapMsg, CoapMsg]] = ???
+
+    /**
+      * @return Responses for requests, and requests from clients.
+      */
     def outDatagrams: Observable[Datagram] = ???
 
+    /**
+      * Each session has own back-pressure for incoming datagrams.
+      */
     def inDatagrams: Channel[Datagram] = ???
 
-
   }
 
-  class CoapClient(val stream: InOut[CoapMsg, CoapMsg], val targetAddress: InetSocketAddress, val initMsg: CoapMsg,
-                   val scheduler: Scheduler) {
-
-  }
+  class CoapClient(val stream: InOut[CoapMsg, CoapMsg], val targetAddress: InetSocketAddress, val initMsg: CoapMsg)
 
 }
 
@@ -55,13 +71,12 @@ class CoapSessions extends BidiProcessor[Datagram, CoapSession, CoapClient, Obse
   override def onOutputMessage(client: CoapClient): ProcessingAction[CoapSession, Observable[Datagram]] = {
     val oldSession = sessions.get(client.targetAddress)
     if (oldSession != null) {
-      client.stream.onError(new IllegalStateException("Session with this address already exists: " + client.targetAddress))
+      oldSession.clients().onNext(client.stream)
       NoAction
     } else {
       val session = new CoapSession(client.targetAddress, client.initMsg)
       sessions.put(client.targetAddress, session)
-      session.stream().subscribe(client.stream)(client.scheduler)
-      client.stream.subscribe(session.stream())(client.scheduler)
+      session.clients().onNext(client.stream)
       PushToOutput(session.outDatagrams)
     }
   }
