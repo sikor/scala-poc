903a18a3882f8ff7bcc4b9a18ba5061c2c8e7909
sikor <p.sikora@avsystem.com>
2015-11-20 13:36:35 +0100

    Coap client and server api draft.

--- core/src/main/scala/streams/io/CoapSessions.scala
+++ core/src/main/scala/streams/io/CoapSessions.scala
@@ -3,11 +3,10 @@ package streams.io
 import java.net.InetSocketAddress
 import java.util
 
-import monifu.concurrent.Scheduler
+import _root_.streams.io.CoapSessions.{IncomingRequests, CoapSession, Request}
 import monifu.reactive.observers.SynchronousObserver
-import streams.bidi.BidiStream.{PushToOutput, NoAction, ProcessingAction, PushToBoth}
-import streams.bidi.{BidiProcessor, InOut}
-import streams.io.CoapSessions.{CoapClient, CoapSession}
+import streams.bidi.BidiProcessor
+import streams.bidi.BidiStream.{NoAction, ProcessingAction, PushToBoth, PushToOutput}
 import streams.io.UdpStream.Datagram
 import monifu.reactive._
 
@@ -28,13 +27,13 @@ object CoapSessions {
     * accessed separately.
     *
     * @param address associated target address
-    * @param initMsg message which initiated the session.
     */
-  class CoapSession(val address: InetSocketAddress, val initMsg: CoapMsg) {
+  class CoapSession(val address: InetSocketAddress) {
 
-    def requests(): Observable[InOut[CoapMsg, CoapMsg]] = ???
+    def server(): Observable[Request] = ???
 
-    def clients(): SynchronousObserver[InOut[CoapMsg, CoapMsg]] = ???
+
+    def client(): SynchronousObserver[Request] = ???
 
     /**
       * @return Responses for requests, and requests from clients.
@@ -48,36 +47,54 @@ object CoapSessions {
 
   }
 
-  class CoapClient(val stream: InOut[CoapMsg, CoapMsg], val targetAddress: InetSocketAddress, val initMsg: CoapMsg)
+  class IncomingRequests(val address: InetSocketAddress, requests: Observable[Request])
+
+  class Request(val address: InetSocketAddress, val request: CoapMsg, val responses: Observer[CoapMsg])
 
 }
 
-class CoapSessions extends BidiProcessor[Datagram, CoapSession, CoapClient, Observable[Datagram]] {
+class CoapSessions extends BidiProcessor[Datagram, IncomingRequests, Request, Observable[Datagram]] {
 
   private val sessions = new util.HashMap[InetSocketAddress, CoapSession]
 
-  override def onInputMessage(datagram: Datagram): ProcessingAction[CoapSession, Observable[Datagram]] = {
+  /**
+    *
+    * @param datagram input datagram from lower layer
+    * @return IncomingRequests which is stream of requests from one IP:PORT. Request has associated response channel.
+    *         If request has many parts (block-wise) than it may not be pushed to subscriber immediately after subscription.
+    *         Nevertheless, the notification about incoming block-wise transfer has to be pushed to run handling on different thread.
+    *         When returned Observable completes and new request arrive from this endpoint than new Observable for this
+    *         endpoint will be returned. Resulting Observable[Observable[Request]\] can be merged and requests handled
+    *         separately.
+    *
+    */
+  override def onInputMessage(datagram: Datagram): ProcessingAction[IncomingRequests, Observable[Datagram]] = {
     val oldSession = sessions.get(datagram.address)
     if (oldSession != null) {
       oldSession.inDatagrams.pushNext(datagram)
       NoAction
     } else {
-      val session = new CoapSession(datagram.address, datagram)
+      val session = new CoapSession(datagram.address)
       sessions.put(datagram.address, session)
-      PushToBoth(session, session.outDatagrams)
+      session.inDatagrams.pushNext(datagram)
+      PushToBoth(new IncomingRequests(datagram.address, session.server()), session.outDatagrams)
     }
   }
 
-  override def onOutputMessage(client: CoapClient): ProcessingAction[CoapSession, Observable[Datagram]] = {
-    val oldSession = sessions.get(client.targetAddress)
-    if (oldSession != null) {
-      oldSession.clients().onNext(client.stream)
+  /**
+    * Clients can send requests and listen for responses.
+    *
+    */
+  override def onOutputMessage(request: Request): ProcessingAction[IncomingRequests, Observable[Datagram]] = {
+    val session = sessions.get(request.address)
+    if (session != null) {
+      session.client().onNext(request)
       NoAction
     } else {
-      val session = new CoapSession(client.targetAddress, client.initMsg)
-      sessions.put(client.targetAddress, session)
-      session.clients().onNext(client.stream)
-      PushToOutput(session.outDatagrams)
+      val newSession = new CoapSession(request.address)
+      sessions.put(request.address, newSession)
+      newSession.client().onNext(request)
+      PushToOutput(newSession.outDatagrams)
     }
   }
 }
